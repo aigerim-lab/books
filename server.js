b5d3e6023d8f1de58db3031b41902db65c173b31
kama025 <kamshatkoszhan.02@gmail.com>
2025-02-09 18:45:01 +0500

    Fixed root directory

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.delete('/books/:id', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //API path 
